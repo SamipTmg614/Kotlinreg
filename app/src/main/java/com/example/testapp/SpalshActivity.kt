ad15871c74b13f54fd5fa39a7b8731c6db0d9e54
samipEDU <240246@softwarica.edu.np>
2025-05-15 11:42:03 +0545

    intent and activity navigation

---
+++ app/src/main/java/com/example/testapp/SpalshActivity.kt
@@ -0,0 +1,60 @@
+package com.example.testapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
+
+class SpalshActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SplashBody()
+        }
+    }
+}
+
+@Composable
+fun SplashBody() {
+    val context = LocalContext.current
+    val activity = context as Activity
+    LaunchedEffect (Unit){
+        delay(2000)
+        val intent = Intent(context, LoginActivity::class.java)
+        context.startActivity(intent)
+        activity.finish()
+    }
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding).fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null
+            )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashPreview() {
+    SplashBody()
+}
